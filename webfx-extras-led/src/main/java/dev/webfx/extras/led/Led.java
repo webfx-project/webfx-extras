264003649f9e0a4fa3ec04d81e155827aad5d2f8
Bruno Salmon <dev.salmonb@gmail.com>
2022-08-14 07:54:08 +0100

    Added Led.setShowBorder() and Led.setShowReflectionWhenNotHighlighted() methods

--- webfx-extras-led/src/main/java/dev/webfx/extras/led/Led.java
+++ webfx-extras-led/src/main/java/dev/webfx/extras/led/Led.java
@@ -23,9 +23,12 @@ import javafx.scene.shape.StrokeLineCap;
  */
 public final class Led extends Region {
 
-    private final Circle ledBorder = new Circle(), ledCentre = new Circle(), highlight = new Circle();
+    private final Circle ledBorder = new Circle(), ledCentre = new Circle(), reflection = new Circle();
     private final Line hLine = new Line(), vLine = new Line();
+    private final Node sign;
     private Color ledColor;
+
+    private boolean showBorder = true, showReflectionWhenNotHighlighted = true;
     private Paint pressedFill, releasedFill;
     private final BooleanProperty highlightedProperty = new SimpleBooleanProperty() {
         @Override
@@ -50,10 +53,10 @@ public final class Led extends Region {
         hLine.setStrokeLineCap(StrokeLineCap.ROUND);
         vLine.setStroke(lineColor);
         vLine.setStrokeLineCap(StrokeLineCap.ROUND);
-        Node sign = plus == Boolean.TRUE ? new Group(hLine, vLine) : hLine;
+        sign = plus == Boolean.TRUE ? new Group(hLine, vLine) : hLine;
         sign.setOpacity(0.6);
-        getChildren().setAll(ledBorder, ledCentre, highlight, sign);
-        highlight.setMouseTransparent(true);
+        getChildren().setAll(ledBorder, ledCentre, reflection, sign);
+        reflection.setMouseTransparent(true);
         sign.setMouseTransparent(true);
         if (plus == null)
             sign.setVisible(false);
@@ -62,17 +65,27 @@ public final class Led extends Region {
     }
 
     public void setColor(Color ledColor) {
-        this.ledColor = ledColor;
-        pressedFill = new LinearGradient(0, 0, 1, 1, true, CycleMethod.NO_CYCLE,
-                new Stop(0.0,  ledColor.deriveColor(0d, 1d, 0.77, 1d)),
-                new Stop(0.49, ledColor.deriveColor(0d, 1d, 0.5,  1d)),
-                new Stop(1.0,  ledColor));
-        releasedFill = new LinearGradient(0, 0, 1, 1, true, CycleMethod.NO_CYCLE,
-                new Stop(0.0,  ledColor.deriveColor(0d, 1d, 0.57, 1d)),
-                new Stop(0.49, ledColor.deriveColor(0d, 1d, 0.4,  1d)),
-                new Stop(1.0,  ledColor.deriveColor(0d, 1d, 0.2,  1d)));
-        ledCentre.setFill(releasedFill);
-        updateHighlight();
+        if (!ledColor.equals(this.ledColor)) {
+            this.ledColor = ledColor;
+            pressedFill = new LinearGradient(0, 0, 1, 1, true, CycleMethod.NO_CYCLE,
+                    new Stop(0.0, ledColor.deriveColor(0d, 1d, 0.77, 1d)),
+                    new Stop(0.49, ledColor.deriveColor(0d, 1d, 0.5, 1d)),
+                    new Stop(1.0, ledColor));
+            releasedFill = new LinearGradient(0, 0, 1, 1, true, CycleMethod.NO_CYCLE,
+                    new Stop(0.0, ledColor.deriveColor(0d, 1d, 0.57, 1d)),
+                    new Stop(0.49, ledColor.deriveColor(0d, 1d, 0.4, 1d)),
+                    new Stop(1.0, ledColor.deriveColor(0d, 1d, 0.2, 1d)));
+            ledCentre.setFill(releasedFill);
+            updateHighlight();
+        }
+    }
+
+    public void setShowBorder(boolean showBorder) {
+        this.showBorder = showBorder;
+    }
+
+    public void setShowReflectionWhenNotHighlighted(boolean showReflectionWhenNotHighlighted) {
+        this.showReflectionWhenNotHighlighted = showReflectionWhenNotHighlighted;
     }
 
     public BooleanProperty highlightedProperty() {
@@ -91,9 +104,11 @@ public final class Led extends Region {
         if (isHighlighted()) {
             ledCentre.setFill(pressedFill);
             ledCentre.setEffect(new DropShadow(ledCentre.getRadius() * 0.3, ledColor));
+            reflection.setVisible(true);
         } else {
             ledCentre.setFill(releasedFill);
             ledCentre.setEffect(null);
+            reflection.setVisible(showReflectionWhenNotHighlighted);
         }
     }
 
@@ -111,19 +126,21 @@ public final class Led extends Region {
         double width = getWidth(), height = getHeight();
         double radius = Math.min(width, height) / 2;
         ledBorder.setRadius(radius);
-        ledCentre.setRadius(0.8 * radius);
-        highlight.setRadius(0.7 * radius);
+        ledCentre.setRadius((showBorder ? 0.8 : 1.0) * radius);
+        reflection.setRadius(0.9 * ledCentre.getRadius());
         innerShadow.setRadius(0.8 * 0.075 / 0.15 * radius);
-        Paint highlightFill = new RadialGradient(0, 0, 0 - highlight.getRadius(), 0 - highlight.getRadius(), highlight.getRadius(), false,
+        Paint highlightFill = new RadialGradient(0, 0, 0 - reflection.getRadius(), 0 - reflection.getRadius(), reflection.getRadius(), false,
                 CycleMethod.NO_CYCLE, new Stop(0.0, Color.WHITE), new Stop(1.0, Color.TRANSPARENT));
-        highlight.setFill(highlightFill);
-        double lineLength = 0.4 * radius;
-        hLine.setStartX(width / 2 - lineLength);
-        hLine.setEndX(width / 2 + lineLength);
-        hLine.setStrokeWidth(0.2 * radius);
-        vLine.setStartY(width / 2 - lineLength);
-        vLine.setEndY(width / 2 + lineLength);
-        vLine.setStrokeWidth(0.2 * radius);
+        reflection.setFill(highlightFill);
+        if (sign.isVisible()) {
+            double lineLength = 0.4 * radius;
+            hLine.setStartX(width / 2 - lineLength);
+            hLine.setEndX(width / 2 + lineLength);
+            hLine.setStrokeWidth(0.2 * radius);
+            vLine.setStartY(width / 2 - lineLength);
+            vLine.setEndY(width / 2 + lineLength);
+            vLine.setStrokeWidth(0.2 * radius);
+        }
         for (Node child : getChildren())
             if (!(child instanceof Group))
                 layoutInArea(child, 0, 0, width, height, 0 , HPos.CENTER, VPos.CENTER);

9128ba18e0ec6817fe5eaaccef740eaf7c1218d8
Bruno Salmon <dev.salmonb@gmail.com>
2023-03-13 17:35:34 +0000

    Added webfx-extras-timelayout(-xxx) modules files

---
+++ webfx-extras-timelayout-calendar/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.extras.timelayout.calendar {
+
+    // Direct dependencies modules
+    requires java.base;
+    requires javafx.base;
+    requires webfx.extras.timelayout;
+    requires webfx.platform.util;
+
+    // Exported packages
+    exports dev.webfx.extras.timelayout.impl.calendar;
+
+}
---
+++ webfx-extras-timelayout/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.extras.timelayout {
+
+    // Direct dependencies modules
+    requires java.base;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires webfx.platform.util;
+
+    // Exported packages
+    exports dev.webfx.extras.timelayout;
+    exports dev.webfx.extras.timelayout.impl;
+
+}

c1eca78d34e040088443bddd1348ea3a9c2eb3e5
Bruno Salmon <dev.salmonb@gmail.com>
2023-06-01 14:26:45 +0100

    Renamed setupBarsLayout() to convertToBlocksThenGroupToBars() in TimeBarUtil

--- webfx-extras-time-layout/src/main/java/dev/webfx/extras/time/layout/bar/TimeBarUtil.java
+++ webfx-extras-time-layout/src/main/java/dev/webfx/extras/time/layout/bar/TimeBarUtil.java
@@ -18,28 +18,27 @@ public class TimeBarUtil {
 
     // LocalDate API
 
-    public static <E, B> void setupBarsLayout(ObservableList<E> entities, Function<E, LocalDate> entityDateReader, Function<E, B> entityToBlockConverter, TimeLayout<LocalDateBar<B>, LocalDate> barsLayout) {
-        TimeBarUtil.<E, B, LocalDate, LocalDateBar<B>>setupTimeLayout(entities, entityDateReader, entityToBlockConverter, LocalDateBar::new, barsLayout);
+    public static <E, B> void convertToBlocksThenGroupToBars(ObservableList<E> entities, Function<E, LocalDate> entityDateReader, Function<E, B> entityToBlockConverter, ObservableList<LocalDateBar<B>> timeBars) {
+        TimeBarUtil.<E, B, LocalDate, LocalDateBar<B>>convertToBlocksThenGroupToBars(entities, entityDateReader, entityToBlockConverter, LocalDateBar::new, timeBars);
     }
 
-    public static <E, B> void setupBarsLayout(ObservableList<E> entities, Function<E, LocalDate> entityDateReader, Function<E, B> entityToBlockConverter, TimeLayout<LocalDateBar<B>, LocalDate> barsLayout, ObservableValue<Boolean> blocksGroupingProperty) {
-        TimeBarUtil.<E, B, LocalDate, LocalDateBar<B>>setupTimeLayout(entities, entityDateReader, entityToBlockConverter, LocalDateBar::new, barsLayout, blocksGroupingProperty);
+    public static <E, B> void convertToBlocksThenGroupToBars(ObservableList<E> entities, Function<E, LocalDate> entityDateReader, Function<E, B> entityToBlockConverter, ObservableList<LocalDateBar<B>> timeBars, ObservableValue<Boolean> blocksGroupingProperty) {
+        TimeBarUtil.<E, B, LocalDate, LocalDateBar<B>>convertToBlocksThenGroupToBars(entities, entityDateReader, entityToBlockConverter, LocalDateBar::new, timeBars, blocksGroupingProperty);
     }
 
     // Generic API
 
-    static <TB extends TimeBar<?, T>, T> void setTimeBarLayoutTimeReaders(TimeLayout<TB, T> timeLayout) {
+    public static <TB extends TimeBar<?, T>, T> void setBarsLayoutTimeReaders(TimeLayout<TB, T> timeLayout) {
         timeLayout.setInclusiveChildStartTimeReader(TimeBar::getStartTime);
         timeLayout.setInclusiveChildEndTimeReader(TimeBar::getEndTime);
     }
 
-    public static <E, B, T, TB extends TimeBar<B, T>> void setupTimeLayout(ObservableList<E> entities, Function<E, T> entityTimeReader, Function<E, B> entityToBlockConverter, TimeBarFactory<B, T, TB> timeBarFactory, TimeLayout<TB, T> timeLayout) {
-        setupTimeLayout(entities, entityTimeReader, entityToBlockConverter, timeBarFactory, timeLayout, null);
+    public static <E, B, T, TB extends TimeBar<B, T>> void convertToBlocksThenGroupToBars(ObservableList<E> entities, Function<E, T> entityTimeReader, Function<E, B> entityToBlockConverter, TimeBarFactory<B, T, TB> timeBarFactory, ObservableList<TB> timeLayout) {
+        convertToBlocksThenGroupToBars(entities, entityTimeReader, entityToBlockConverter, timeBarFactory, timeLayout, null);
     }
 
-    public static <E, B, T, TB extends TimeBar<B, T>> void setupTimeLayout(ObservableList<E> entities, Function<E, T> entityTimeReader, Function<E, B> entityToBlockConverter, TimeBarFactory<B, T, TB> timeBarFactory, TimeLayout<TB, T> timeLayout, ObservableValue<Boolean> blocksGroupingProperty) {
-        setTimeBarLayoutTimeReaders(timeLayout);
-        bindTimeBarsFromEntities(entities, entityTimeReader, entityToBlockConverter, timeBarFactory, timeLayout.getChildren(), blocksGroupingProperty);
+    public static <E, B, T, TB extends TimeBar<B, T>> void convertToBlocksThenGroupToBars(ObservableList<E> entities, Function<E, T> entityTimeReader, Function<E, B> entityToBlockConverter, TimeBarFactory<B, T, TB> timeBarFactory, ObservableList<TB> timeLayout, ObservableValue<Boolean> blocksGroupingProperty) {
+        bindTimeBarsFromEntities(entities, entityTimeReader, entityToBlockConverter, timeBarFactory, timeLayout, blocksGroupingProperty);
     }
 
     public static <E, B, T, TB extends TimeBar<B, T>> void bindTimeBarsFromEntities(ObservableList<E> entities, Function<E, T> entityTimeReader, Function<E, B> entityToBlockConverter, TimeBarFactory<B, T, TB> timeBarFactory, ObservableList<TB> timeBarObservableList) {

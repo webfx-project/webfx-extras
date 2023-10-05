e2550801c7ef44e4085dae85b8d6e927be97430d
Bruno Salmon <dev.salmonb@gmail.com>
2023-10-05 11:26:49 +0100

    Added webfx-extras-switches

---
+++ webfx-extras-switches/src/main/java/dev/webfx/extras/switches/Switch.java
@@ -0,0 +1,126 @@
+package dev.webfx.extras.switches;
+
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.WritableValue;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+
+/**
+ * @author Bruno Salmon
+ */
+public class Switch extends Pane {
+
+    private final static double RADIUS = 10, WIDTH = 45;
+    private final Circle switchKnob;
+
+    private final BooleanProperty selectedProperty = new SimpleBooleanProperty() {
+        @Override
+        protected void invalidated() {
+            updateSwitchUi(true);
+        }
+    };
+
+    private final ObjectProperty<Paint> selectedBackgroundFillProperty = new SimpleObjectProperty<>(Color.web("#23EA00")) {
+        @Override
+        protected void invalidated() {
+            updateSwitchUi(true);
+        }
+    };
+
+    private final ObjectProperty<Paint> unselectedBackgroundFillProperty = new SimpleObjectProperty<>(Color.LIGHTGRAY) {
+        @Override
+        protected void invalidated() {
+            updateSwitchUi(true);
+        }
+    };
+
+    private final ObjectProperty<Paint> backgroundFillProperty = new SimpleObjectProperty<>() {
+        @Override
+        protected void invalidated() {
+            setBackground(new Background(new BackgroundFill(get(), new CornerRadii(RADIUS), null)));
+        }
+    };
+
+    public Switch() {
+        super(new Circle(RADIUS - 3));
+        switchKnob = (Circle) getChildren().get(0);
+        switchKnob.setLayoutY(RADIUS);
+        setKnobFill(Color.WHITE);
+        setMinSize(WIDTH, 2 * RADIUS);
+        setMaxSize(WIDTH, 2 * RADIUS);
+        setOnMouseClicked(e -> setSelected(!isSelected()));
+        updateSwitchUi(false);
+    }
+
+    public void setKnobFill(Paint fill) {
+        switchKnob.setFill(fill);
+    }
+
+    public boolean isSelected() {
+        return selectedProperty.get();
+    }
+
+    public BooleanProperty selectedProperty() {
+        return selectedProperty;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selectedProperty.set(selected);
+    }
+
+    public Paint getUnselectedBackgroundFill() {
+        return unselectedBackgroundFillProperty.get();
+    }
+
+    public ObjectProperty<Paint> unselectedBackgroundFillProperty() {
+        return unselectedBackgroundFillProperty;
+    }
+
+    public void setUnselectedBackgroundFill(Paint unselectedBackgroundFill) {
+        this.unselectedBackgroundFillProperty.set(unselectedBackgroundFill);
+    }
+
+    public Paint getSelectedBackgroundFill() {
+        return selectedBackgroundFillProperty.get();
+    }
+
+    public ObjectProperty<Paint> selectedBackgroundFillProperty() {
+        return selectedBackgroundFillProperty;
+    }
+
+    public void setSelectedBackgroundFill(Paint selectedBackgroundFill) {
+        this.selectedBackgroundFillProperty.set(selectedBackgroundFill);
+    }
+
+    private void updateSwitchUi(boolean animate) {
+        double layoutX = isSelected() ? WIDTH - RADIUS : RADIUS;
+        Paint backgroundFill = isSelected() ? getSelectedBackgroundFill() : getUnselectedBackgroundFill();
+        if (!animate) {
+            switchKnob.setLayoutX(layoutX);
+            backgroundFillProperty.set(backgroundFill);
+        } else {
+            animateProperty(200, switchKnob.layoutXProperty(), layoutX);
+            animateProperty(200, backgroundFillProperty, backgroundFill);
+        }
+    }
+
+    static <T> Timeline animateProperty(int durationMillis, WritableValue<T> target, T endValue) {
+        Timeline timeline = new Timeline(new KeyFrame(new Duration(durationMillis), new KeyValue(target, endValue, Interpolator.EASE_BOTH)));
+        timeline.play();
+        return timeline;
+    }
+
+}

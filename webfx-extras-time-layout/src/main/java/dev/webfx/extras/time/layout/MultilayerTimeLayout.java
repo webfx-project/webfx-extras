356507de8db78c4cf0099e788cfb0b179ef0e961
Bruno Salmon <dev.salmonb@gmail.com>
2024-05-02 19:58:03 +0100

    Made MultilayerTimeLayout extends HasTimeProjector

--- webfx-extras-time-layout/src/main/java/dev/webfx/extras/time/layout/MultilayerTimeLayout.java
+++ webfx-extras-time-layout/src/main/java/dev/webfx/extras/time/layout/MultilayerTimeLayout.java
@@ -2,9 +2,10 @@ package dev.webfx.extras.time.layout;
 
 import dev.webfx.extras.layer.interact.InteractiveMultilayer;
 import dev.webfx.extras.time.layout.impl.MultilayerTimeLayoutImpl;
+import dev.webfx.extras.time.projector.HasTimeProjector;
 import dev.webfx.extras.time.window.ListenableTimeWindow;
 
-public interface MultilayerTimeLayout<T> extends ListenableTimeWindow<T>, CanLayout, InteractiveMultilayer<TimeLayout<?, T>> {
+public interface MultilayerTimeLayout<T> extends ListenableTimeWindow<T>, CanLayout, HasTimeProjector<T>, InteractiveMultilayer<TimeLayout<?, T>> {
 
     static <T> MultilayerTimeLayout<T> create() {
         return new MultilayerTimeLayoutImpl<>();

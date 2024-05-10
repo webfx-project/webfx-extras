855478ac793d656da4c13dfdd38b6d28f90dc6bc
Bruno Salmon <dev.salmonb@gmail.com>
2024-05-10 16:00:41 +0100

    Moved Carrousel to new webfx-extras-carrousel module

--- webfx-extras-carrousel/src/main/java/dev/webfx/extras/carrousel/Carrousel.java
+++ webfx-extras-carrousel/src/main/java/dev/webfx/extras/carrousel/Carrousel.java
@@ -1,17 +1,20 @@
-package dev.webfx.extras.panes;
+package dev.webfx.extras.carrousel;
 
+import dev.webfx.extras.panes.TransitionPane;
 import dev.webfx.kit.util.properties.ObservableLists;
 import dev.webfx.platform.util.Arrays;
 import dev.webfx.platform.util.collection.Collections;
 import javafx.application.Platform;
+import javafx.beans.property.ReadOnlyBooleanProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
@@ -26,9 +29,10 @@ public final class Carrousel {
     private final ObservableList<Supplier<Node>> slideSuppliers = FXCollections.observableArrayList();
     private final ObservableList<Circle> dots = ObservableLists.map(slideSuppliers, n -> createDot());
     private int displayedSlideIndex;
+    private boolean loop = true;
     private final TransitionPane transitionPane = new TransitionPane();
     private final HBox dotsBox = new HBox(10);
-    private final BorderPane container = new BorderPane(transitionPane);
+    private final VBox container = new VBox(transitionPane, dotsBox);
 
     public Carrousel() {
         transitionPane.setMaxWidth(Double.MAX_VALUE);
@@ -36,7 +40,7 @@ public final class Carrousel {
         dotsBox.setAlignment(Pos.CENTER);
         dotsBox.getStyleClass().add("dots");
         ObservableLists.bind(dotsBox.getChildren(), dots);
-        container.setBottom(dotsBox);
+        //container.setBottom(dotsBox);
         dotsBox.setOnMouseClicked(e -> {
             if (e.getX() < dotsBox.getWidth() / 2)
                 moveBackward();
@@ -66,8 +70,16 @@ public final class Carrousel {
         setSlideSuppliers(Arrays.map(slides, slide -> () -> slide, Supplier[]::new));
     }
 
-    public Node getContainer() {
+    public Region getContainer() {
         return container;
+    }
+
+    public boolean isLoop() {
+        return loop;
+    }
+
+    public void setLoop(boolean loop) {
+        this.loop = loop;
     }
 
     public void moveForward() {
@@ -80,8 +92,18 @@ public final class Carrousel {
 
     private void displaySlide(int index) {
         transitionPane.setDirection(index > displayedSlideIndex ? HPos.LEFT : HPos.RIGHT);
-        index = (index + slideSuppliers.size()) % slideSuppliers.size();
-        if (index != displayedSlideIndex || transitionPane.getContent() == null) {
+        int size = slideSuppliers.size();
+        // index correction when it's out of the slides range (i.e. < 0 or >= size)
+        if (loop)
+            index = (index + size) % size;
+        else {
+
+            if (index < 0)
+                index = 0;
+            else if (index >= size)
+                index = size - 1;
+        }
+        if (index != displayedSlideIndex || transitionPane.getContent() == null && !isTransiting()) {
             displayedSlideIndex = index;
             Node content = slideSuppliers.get(index).get();
             transitionPane.setContent(content);
@@ -91,6 +113,10 @@ public final class Carrousel {
 
     public boolean isTransiting() {
         return transitionPane.isTransiting();
+    }
+
+    public ReadOnlyBooleanProperty transitingProperty() {
+        return transitionPane.transitingProperty();
     }
 
     public Node getDisplayedSlide() {
@@ -111,9 +137,9 @@ public final class Carrousel {
     private void updateDotsStyleClass() {
         for (int i = 0; i < dots.size(); i++) {
             boolean isSelected = i == displayedSlideIndex;
-            //dots.get(i).getStyleClass().setAll(isSelected ? "selected" : "unselected");
             // Temporarily hardcoded (to replace with CSS)
             dots.get(i).setFill(isSelected ? Color.WHITE : Color.GRAY);
+            //dots.get(i).getStyleClass().setAll(isSelected ? "selected" : "unselected");
         }
     }
 
--- webfx-extras-carrousel/src/main/java/module-info.java
+++ webfx-extras-carrousel/src/main/java/module-info.java
@@ -1,15 +1,15 @@
 // File managed by WebFX (DO NOT EDIT MANUALLY)
 
-module webfx.extras.panes {
+module webfx.extras.carrousel {
 
     // Direct dependencies modules
     requires javafx.base;
     requires javafx.graphics;
-    requires webfx.extras.util.animation;
+    requires webfx.extras.panes;
     requires webfx.kit.util;
     requires webfx.platform.util;
 
     // Exported packages
-    exports dev.webfx.extras.panes;
+    exports dev.webfx.extras.carrousel;
 
 }

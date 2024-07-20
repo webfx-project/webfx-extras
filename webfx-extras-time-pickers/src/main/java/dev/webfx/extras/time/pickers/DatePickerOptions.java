78f1d149ea6b735196e6d7ba1b6dbb3c5faa05f9
Bruno Salmon <dev.salmonb@gmail.com>
2024-07-20 17:54:23 +0100

    Updated DatesPicker API (renamed DatePicker)

---
+++ webfx-extras-time-pickers/src/main/java/dev/webfx/extras/time/pickers/DatePickerOptions.java
@@ -0,0 +1,60 @@
+package dev.webfx.extras.time.pickers;
+
+import java.time.YearMonth;
+
+/**
+ * @author Bruno Salmon
+ */
+public final class DatePickerOptions {
+
+    private boolean applyBorderStyle = true;
+    private boolean applyMaxSize = true;
+    private YearMonth initialDisplayedYearMonth;
+    private boolean multipleSelectionAllowed = false;
+    private boolean pastDatesSelectionAllowed = true;
+
+    public boolean isApplyBorderStyle() {
+        return applyBorderStyle;
+    }
+
+    public DatePickerOptions setApplyBorderStyle(boolean applyBorderStyle) {
+        this.applyBorderStyle = applyBorderStyle;
+        return this;
+    }
+
+    public boolean isApplyMaxSize() {
+        return applyMaxSize;
+    }
+
+    public DatePickerOptions setApplyMaxSize(boolean applyMaxSize) {
+        this.applyMaxSize = applyMaxSize;
+        return this;
+    }
+
+    public YearMonth getInitialDisplayedYearMonth() {
+        return initialDisplayedYearMonth;
+    }
+
+    public DatePickerOptions setInitialDisplayedYearMonth(YearMonth initialDisplayedYearMonth) {
+        this.initialDisplayedYearMonth = initialDisplayedYearMonth;
+        return this;
+    }
+
+    public boolean isMultipleSelectionAllowed() {
+        return multipleSelectionAllowed;
+    }
+
+    public DatePickerOptions setMultipleSelectionAllowed(boolean multipleSelectionAllowed) {
+        this.multipleSelectionAllowed = multipleSelectionAllowed;
+        return this;
+    }
+
+    public boolean isPastDatesSelectionAllowed() {
+        return pastDatesSelectionAllowed;
+    }
+
+    public DatePickerOptions setPastDatesSelectionAllowed(boolean pastDatesSelectionAllowed) {
+        this.pastDatesSelectionAllowed = pastDatesSelectionAllowed;
+        return this;
+    }
+}

311baeca07820888d0fe70141769057fdb3848e2
Bruno Salmon <dev.salmonb@gmail.com>
2024-07-20 15:09:02 +0100

    Added webfx-extras-time-datepickers

---
+++ webfx-extras-time-datepickers/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.extras.time.datepickers {
+
+    // Direct dependencies modules
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires webfx.extras.theme;
+    requires webfx.extras.time;
+    requires webfx.extras.time.format;
+    requires webfx.extras.time.layout;
+    requires webfx.extras.time.layout.calendar;
+    requires webfx.platform.util;
+    requires webfx.kit.util;
+
+    // Exported packages
+    exports dev.webfx.extras.time.datepickers;
+
+}

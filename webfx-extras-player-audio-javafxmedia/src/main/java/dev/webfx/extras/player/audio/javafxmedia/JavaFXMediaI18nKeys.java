d992e3280aa3cd42ee32dc0fae6169e85904c953
David <david.dev.kbs@kadampa.net>
2024-12-20 15:25:42 +0000

    Updated AudioMediaView to remove I18n dependencies

--- webfx-extras-player-audio-javafxmedia/src/main/java/dev/webfx/extras/player/audio/javafxmedia/JavaFXMediaI18nKeys.java
+++
@@ -1,9 +0,0 @@
-// File managed by WebFX (DO NOT EDIT MANUALLY)
-package dev.webfx.extras.player.audio.javafxmedia;
-
-public interface JavaFXMediaI18nKeys {
-
-    String AbrSecond = "AbrSecond";
-    String ChooseAnAudioTrackForBehind = "ChooseAnAudioTrackForBehind";
-
-}

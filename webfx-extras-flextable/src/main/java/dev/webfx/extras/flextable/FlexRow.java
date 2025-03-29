9d1c9fca9cb9d364ff7ba1b9980edbc2388dd127
Bruno Salmon <dev.salmonb@gmail.com>
2025-03-29 10:45:24 +0000

    Added webfx-extras-flextable module (initial commit)

---
+++ webfx-extras-flextable/src/main/java/dev/webfx/extras/flextable/FlexRow.java
@@ -0,0 +1,12 @@
+package dev.webfx.extras.flextable;
+
+import javafx.scene.Node;
+
+/**
+ * @author Bruno Salmon
+ */
+public interface FlexRow {
+
+    Node getCellNode(FlexColumn flexColumn);
+
+}
---
+++ webfx-extras-flextable/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.extras.flextable {
+
+    // Direct dependencies modules
+    requires javafx.base;
+    requires javafx.graphics;
+    requires webfx.extras.panes;
+    requires webfx.kit.util;
+
+    // Exported packages
+    exports dev.webfx.extras.flextable;
+
+}

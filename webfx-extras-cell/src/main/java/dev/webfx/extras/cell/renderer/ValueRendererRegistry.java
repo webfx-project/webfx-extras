cdcb8b655db612af15e89ea92fcd17d6958118ed
Bruno Salmon <dev.salmonb@gmail.com>
2025-04-13 21:56:13 +0100

    Added "label", "wrappedLabel" & "ellipsisLabel" default renderers in ValueRendererRegistry

--- webfx-extras-cell/src/main/java/dev/webfx/extras/cell/renderer/ValueRendererRegistry.java
+++ webfx-extras-cell/src/main/java/dev/webfx/extras/cell/renderer/ValueRendererRegistry.java
@@ -1,5 +1,9 @@
 package dev.webfx.extras.cell.renderer;
 
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,6 +20,51 @@ public final class ValueRendererRegistry {
 
     public static ValueRenderer getValueRenderer(String name) {
         return REGISTERED_VALUE_RENDERERS.get(name);
-    };
+    }
 
+    static {
+        // Registering default generic renderers
+        registerValueRenderer("boolean", BooleanRenderer.SINGLETON);
+        registerValueRenderer("text", TextRenderer.SINGLETON);
+        registerValueRenderer("image", ImageRenderer.SINGLETON);
+        registerValueRenderer("imageText", ImageTextRenderer.SINGLETON);
+        registerValueRenderer("label", (value, context) -> renderLabel(value, false, false));
+        registerValueRenderer("wrappedLabel", (value, context) -> renderLabel(value, true, false));
+        registerValueRenderer("ellipsisLabel", (value, context) -> renderLabel(value, true, true));
+        registerValueRenderer("html", HtmlTextRenderer.SINGLETON);
+    }
+
+    private static Label renderLabel(Object value, boolean autoWrap, boolean ellipsis) {
+        Label label = new Label();
+        if (autoWrap) {
+            label.setWrapText(true);
+            // Note that setting wrapText = true is not enough to make the label automatically wrap the text, the other
+            // condition being to constraint either 1) the width or 2) the height of the label as follows:
+            // 1) label.prefWidthProperty().bind(vBox.widthProperty());
+            // 2) label.setMinHeight(Region.USE_PREF_SIZE);
+            // We choose 2) as it doesn't need a reference to the container. In addition, it's not sure that the label
+            // needs to have the exact same width as the container (it may have margin or be laid out differently).
+            // So 2) is definitely more generic.
+            label.setMinHeight(Region.USE_PREF_SIZE);
+            // This is the general default behaviour for "wrappedLabel" and "ellipsisLabel", but some renderers may
+            // wish a different behaviour. In that case, they can call removePossibleLabelAutoWrap() method (see for
+            // example VisualGridTableSkin.setCellContent() method).
+        }
+        if (ellipsis)
+            label.getStyleClass().add("ellipsis");
+        ValueApplier.applyValue(value, label.textProperty());
+        return label;
+    }
+
+    public static void removePossibleLabelAutoWrap(Node node) {
+        if (node instanceof Label label) {
+            removePossibleLabelAutoWrap(label);
+        }
+    }
+
+    public static void removePossibleLabelAutoWrap(Label label) {
+        if (label.isWrapText() && label.getMinHeight() == Region.USE_PREF_SIZE) {
+            label.setMinHeight(0);
+        }
+    }
 }

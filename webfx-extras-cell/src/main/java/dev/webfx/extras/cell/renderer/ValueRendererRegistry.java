d2f5e6c291d15ca6b38cec384fed288dd1015859
Bruno Salmon <dev.salmonb@gmail.com>
2025-04-28 21:02:03 +0100

    Added renderLabeled() utility methods in ValueRendererRegistry

--- webfx-extras-cell/src/main/java/dev/webfx/extras/cell/renderer/ValueRendererRegistry.java
+++ webfx-extras-cell/src/main/java/dev/webfx/extras/cell/renderer/ValueRendererRegistry.java
@@ -2,6 +2,7 @@ package dev.webfx.extras.cell.renderer;
 
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
 import javafx.scene.layout.Region;
 
 import java.util.HashMap;
@@ -35,25 +36,32 @@ public final class ValueRendererRegistry {
     }
 
     public static Label renderLabel(Object value, boolean autoWrap, boolean ellipsis) {
-        Label label = new Label();
+        return renderLabeled(new Label(), value, autoWrap, ellipsis);
+    }
+
+    public static <L extends Labeled> L renderLabeled(L labeled, Object value, boolean autoWrap, boolean ellipsis) {
+        ValueApplier.applyValue(value, labeled.textProperty());
+        return renderLabeled(labeled, autoWrap, ellipsis);
+    }
+
+    public static <L extends Labeled> L renderLabeled(L labeled, boolean autoWrap, boolean ellipsis) {
         if (autoWrap) {
-            label.setWrapText(true);
-            // Note that setting wrapText = true is not enough to make the label automatically wrap the text, the other
-            // condition being to constraint either 1) the width or 2) the height of the label as follows:
-            // 1) label.prefWidthProperty().bind(vBox.widthProperty());
-            // 2) label.setMinHeight(Region.USE_PREF_SIZE);
-            // We choose 2) as it doesn't need a reference to the container. In addition, it's not sure that the label
+            labeled.setWrapText(true);
+            // Note that setting wrapText = true is not enough to make the labeled automatically wrap the text, the other
+            // condition being to constraint either 1) the width or 2) the height of the labeled as follows:
+            // 1) labeled.prefWidthProperty().bind(vBox.widthProperty());
+            // 2) labeled.setMinHeight(Region.USE_PREF_SIZE);
+            // We choose 2) as it doesn't need a reference to the container. In addition, it's not sure that the labeled
             // needs to have the exact same width as the container (it may have margin or be laid out differently).
             // So 2) is definitely more generic.
-            label.setMinHeight(Region.USE_PREF_SIZE);
+            labeled.setMinHeight(Region.USE_PREF_SIZE);
             // This is the general default behavior for "wrappedLabel" and "ellipsisLabel", but some renderers may
             // wish a different behavior. In that case, they can call removePossibleLabelAutoWrap() method (see for
             // example VisualGridTableSkin.setCellContent() method).
         }
         if (ellipsis)
-            label.getStyleClass().add("ellipsis");
-        ValueApplier.applyValue(value, label.textProperty());
-        return label;
+            labeled.getStyleClass().add("ellipsis");
+        return labeled;
     }
 
     public static void removePossibleLabelAutoWrap(Node node) {

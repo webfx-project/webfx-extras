3b1672ebb16d0ab8d8f96a3141e18fe6fd7561aa
davidhello7772 <david.dev.kbs@kadampa.net>
2025-11-14 17:00:31 +0000

    Improved the UX Design on DialogContent and add new Helpers

---
+++ webfx-extras-util-dialog/src/main/java/dev/webfx/extras/util/dialog/i18n/DialogI18nKeys.java
@@ -0,0 +1,23 @@
+package dev.webfx.extras.util.dialog.i18n;
+
+/**
+ * I18n keys for DialogContent UI text.
+ * These keys map to translations in webfx-extras-util-dialog_*.properties files.
+ *
+ * @author Claude Code
+ */
+public interface DialogI18nKeys {
+
+    // Button text
+    String ButtonOk = "ButtonOk";
+    String ButtonCancel = "ButtonCancel";
+    String ButtonYes = "ButtonYes";
+    String ButtonNo = "ButtonNo";
+    String ButtonSave = "ButtonSave";
+    String ButtonConfirm = "ButtonConfirm";
+
+    // Error dialog technical details
+    String ErrorDetails = "ErrorDetails";
+    String ErrorCode = "ErrorCode";
+    String ErrorTimestamp = "ErrorTimestamp";
+}
